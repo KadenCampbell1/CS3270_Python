a075c1629868cea205e51aa2b5c6186dc9d9b587
Kaden(LenLap) <10836246@uvu.edu>
2023-01-24 09:58:36 -0700

    Worked on exercise 2

---
+++ Exercise2/wc.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+with open("Strings.txt") as IN_FILE:
+    data = ""
+    for i in IN_FILE.read().lower():
+        if i.isalpha() or i == "'" or i.isspace():
+            data += i
+        else:
+            data += " "
+
+data = data.strip().split()
+d = Counter(data)
+d = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
+
+length = 0
+for keys in d:
+    if len(keys) > length:
+        length = len(keys)
+for keys in d:
+    value = keys + ":"
+    print(f"{value:>{length + 1}} {d[keys]}")

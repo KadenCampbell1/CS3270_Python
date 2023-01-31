d14caf0433d66f85fe4ea3a1c5145136a2459191
Kaden(LenLap) <10836246@uvu.edu>
2023-01-31 14:20:34 -0700

    started exercise 3

--- Exercise3/dec.py
+++ Exercise3/dec.py
@@ -1,10 +1,22 @@
-# @track
+import functools
+
+
+def trace(f):
+    @functools.wraps(f)
+    def wrapper(*args1, **args2):
+        print(f.__name__, 'with', args1, args2)
+        return f(*args1, **args2)
+    return wrapper
+
+
+@trace
 def fib(n):
     return n if n in (0, 1) else fib(n - 1) + fib(n - 2)
 
 
 def main():
-    print(fib(10), "count =", fib.count)
+    # print(fib(10), "count =", fib.count)
+    print(fib(7))
 
 
 if __name__ == "__main__":
